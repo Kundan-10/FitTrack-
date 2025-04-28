7ab84eb3eda22e3978d6092e40179f75c0397669
Kundan-10 <kundanwakudkar10@gmail.com>
2025-04-28 09:26:18 +0530

    adding cronejob

--- FitTrack/src/main/java/com/fittrack/FitTrack/repository/WorkoutRepo.java
+++ FitTrack/src/main/java/com/fittrack/FitTrack/repository/WorkoutRepo.java
@@ -3,8 +3,11 @@ package com.fittrack.FitTrack.repository;
 import com.fittrack.FitTrack.models.Workout;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public interface WorkoutRepo extends JpaRepository<Workout, Integer> {
     List<Workout> findByUserId(Integer id);
+
+    boolean existsByUserIdAndWorkoutDateAfter(Integer id, LocalDate localDate);
 }
---
+++ FitTrack/src/main/java/com/fittrack/FitTrack/serviceimpl/ReminderService.java
@@ -0,0 +1,54 @@
+package com.fittrack.FitTrack.serviceimpl;
+
+import com.fittrack.FitTrack.enums.NotificationStatus;
+import com.fittrack.FitTrack.models.Notification;
+import com.fittrack.FitTrack.models.User;
+import com.fittrack.FitTrack.repository.NotificationRepository;
+import com.fittrack.FitTrack.repository.UserRepo;
+import com.fittrack.FitTrack.repository.WorkoutRepo;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class ReminderService {
+    private final UserRepo userRepository;
+    private final WorkoutRepo workoutRepository;
+    private final NotificationRepository notificationRepository;
+
+    /**
+     * CRON job that runs daily at 12 AM to remind inactive users
+     */
+    @Scheduled(cron = "0 0 0 * * ?")
+    public void sendWorkoutReminderToInactiveUsers() {
+        log.info("CRON Job started: Checking inactive users...");
+
+        List<User> users = userRepository.findAll();
+
+        for (User user : users) {
+            boolean isActiveRecently = workoutRepository.existsByUserIdAndWorkoutDateAfter(
+                    user.getId(),
+                    LocalDate.now().minusDays(3)
+            );
+
+            if (!isActiveRecently) {
+                Notification notification = Notification.builder()
+                        .user(user)
+                        .message("🏃‍♂️ Stay active! You haven't logged any workout recently. Keep moving!")
+                        .isRead(false)
+                        .build();
+
+                notificationRepository.save(notification);
+                log.info("Reminder sent to user: {}", user.getEmail());
+            }
+        }
+
+        log.info("CRON Job completed: Workout reminders sent.");
+    }
+}

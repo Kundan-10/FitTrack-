e75042b9ae01a0b7e690193dcb3d45ff168d2b30
Kundan-10 <kundanwakudkar10@gmail.com>
2025-04-28 08:52:13 +0530

    add role for creating challenge

--- FitTrack/src/main/java/com/fittrack/FitTrack/service/ChallengeService.java
+++ FitTrack/src/main/java/com/fittrack/FitTrack/service/ChallengeService.java
@@ -1,13 +1,14 @@
 package com.fittrack.FitTrack.service;
 
 import com.fittrack.FitTrack.exception.ChallengeException;
+import com.fittrack.FitTrack.exception.UserException;
 import com.fittrack.FitTrack.models.Challenge;
 
 import java.util.List;
 
 public interface ChallengeService {
 
-    Challenge createChallenge(Challenge challenge) throws ChallengeException;
+    Challenge createChallenge(Challenge challenge) throws ChallengeException, UserException;
 
     List<Challenge> getAllChallenges();
 
--- FitTrack/src/main/java/com/fittrack/FitTrack/serviceimpl/ChallengeServiceImpl.java
+++ FitTrack/src/main/java/com/fittrack/FitTrack/serviceimpl/ChallengeServiceImpl.java
@@ -1,9 +1,13 @@
 package com.fittrack.FitTrack.serviceimpl;
 
+import com.fittrack.FitTrack.enums.Role;
 import com.fittrack.FitTrack.exception.ChallengeException;
+import com.fittrack.FitTrack.exception.UserException;
 import com.fittrack.FitTrack.models.Challenge;
+import com.fittrack.FitTrack.models.User;
 import com.fittrack.FitTrack.repository.ChallengeRepository;
 import com.fittrack.FitTrack.service.ChallengeService;
+import com.fittrack.FitTrack.utils.JwtUtils;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -13,9 +17,12 @@ import java.util.List;
 @RequiredArgsConstructor
 public class ChallengeServiceImpl implements ChallengeService {
     private  final ChallengeRepository challengeRepository;
+    private final JwtUtils jwtUtils;
 
     @Override
-    public Challenge createChallenge(Challenge challenge) throws ChallengeException {
+    public Challenge createChallenge(Challenge challenge) throws ChallengeException, UserException {
+        User authUser = jwtUtils.getAuthenticatedUser();
+        if(authUser.getRole() != Role.ADMIN) throw new UserException("Unauthorized for user");
         if (challenge.getStartDate().isAfter(challenge.getEndDate())) {
             throw new ChallengeException("Start date must be before end date.");
         }
